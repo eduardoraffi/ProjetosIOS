e3cb6e0b14eb8fc7e3e62d60883beef5bf25bd0c
Eduardo Jeronymo Goulart Raffi <eraffi@ciandt.com>
2017-08-01 17:32:35 -0300

    Fim aula 94

--- Minhas Viagens/Minhas Viagens/MapViewController.swift
+++ Minhas Viagens/Minhas Viagens/MapViewController.swift
@@ -9,11 +9,45 @@
 import Foundation
 import MapKit
 
-class MapViewController: UIViewController{
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var map: MKMapView!
+    var localizationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configurateLocalizationManager()
+    }
+    
+    func configurateLocalizationManager() {
+        
+        localizationManager.delegate = self
+        localizationManager.desiredAccuracy = kCLLocationAccuracyBest
+        localizationManager.requestWhenInUseAuthorization()
+        localizationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse{
+            
+            let alertController = UIAlertController(title: "Localization permission", message: "Permission needed to access your localization! Please authorize", preferredStyle: .alert)
+            let configActions = UIAlertAction(title: "Open Configs", style: .default, handler: {
+                (alertaConfiguracoes) in
+                if let config = NSURL(string: UIApplicationOpenSettingsURLString){
+                    UIApplication.shared.open(config as URL)
+                }
+            })
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            
+            alertController.addAction(configActions)
+            alertController.addAction(cancelAction)
+            
+            present(alertController, animated: true,completion: nil)
+            
+        }
+        
+    
     }
     
     override func didReceiveMemoryWarning() {
